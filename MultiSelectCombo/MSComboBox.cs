92ee30ecce53c4c487b87e25b64bbaf0cbf041a1
Jeff Kelly <9146472+jekelly@users.noreply.github.com>
2023-08-09 23:39:07 -0700

    hook up a shady custom collectionviewsource

--- MultiSelectCombo/MSComboBox.cs
+++ MultiSelectCombo/MSComboBox.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,9 +49,124 @@ namespace MultiSelectCombo
     /// </summary>
     public class MSComboBox : ComboBox
     {
+        private readonly ObservableCollection<object> _selectedItems = new ObservableCollection<object>();
+
         static MSComboBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MSComboBox), new FrameworkPropertyMetadata(typeof(MSComboBox)));
+            ComboBox.ItemsSourceProperty.AddOwner(typeof(MSComboBox), new FrameworkPropertyMetadata(OnItemsSourceChanged));
+        }
+
+        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MSComboBox msc = (MSComboBox)d;
+
+            if (e.NewValue is TestCollectionView tcv)
+            {
+                foreach (var x in tcv)
+                {
+                    ((INotifyPropertyChanged)x).PropertyChanged += (o, ev) =>
+                    {
+                        if (ev.PropertyName == "IsSelected")
+                        {
+                            if (o is TestCollectionView.SelectableObject so)
+                            {
+                                if (so.IsSelected)
+                                {
+                                    msc._selectedItems.Add(so.Object);
+                                }
+                                else
+                                {
+                                    msc._selectedItems.Remove(so.Object);
+                                }
+                            }
+                        }
+                    };
+                }
+            }
+        }
+
+        public ObservableCollection<object> SelectedObjects => _selectedItems;
+    }
+
+    public class TestCollectionView : CollectionView
+    {
+        private readonly Dictionary<object, SelectableObject> _map = new Dictionary<object, SelectableObject>();
+
+        private SelectableObject GetOrCreate(object o)
+        {
+            if (!_map.TryGetValue(o, out var so))
+            {
+                so = new SelectableObject() { IsSelected = false, Object = o };
+                _map.Add(o, so);
+            }
+
+            return so;
+        }
+
+        public TestCollectionView(IEnumerable collection) : base(collection)
+        {
+        }
+
+        protected override IEnumerator GetEnumerator()
+        {
+            return new TransformingEnumerator(this, base.GetEnumerator());
+        }
+
+        class TransformingEnumerator : IEnumerator
+        {
+            private readonly TestCollectionView tcv;
+            private readonly IEnumerator enumerator;
+
+            public TransformingEnumerator(TestCollectionView tcv, IEnumerator enumerator)
+            {
+                this.tcv = tcv;
+                this.enumerator = enumerator;
+            }
+
+            public object Current => tcv.GetOrCreate(enumerator.Current);
+
+            public bool MoveNext() => enumerator.MoveNext();
+
+            public void Reset() => enumerator.Reset();
+        }
+
+        public override object CurrentItem => this.GetOrCreate(base.CurrentItem);
+
+        public override object GetItemAt(int index) => this.GetOrCreate(base.GetItemAt(index));
+
+        public class SelectableObject : INotifyPropertyChanged
+        {
+            private bool _isSelected;
+            private object _object;
+            public bool IsSelected
+            {
+                get => _isSelected;
+                set
+                {
+                    if (_isSelected != value)
+                    {
+                        _isSelected = value;
+                        OnPropertyChanged(nameof(IsSelected));
+                    }
+                }
+            }
+            public object Object
+            {
+                get => _object;
+                set
+                {
+                    if (_object != value)
+                    {
+                        _object = value;
+                        OnPropertyChanged(nameof(Object));
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
